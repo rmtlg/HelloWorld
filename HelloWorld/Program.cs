a6ea695bc0111992d9a85823714be95268473d61
Larry Grantham <Larry.Grantham@milliken.com>
2018-12-28 09:31:51 -0500

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!!");
+
+            Console.Write("\n<Press any key to exit>");
+            Console.ReadKey(true);
+        }
+    }
+}
